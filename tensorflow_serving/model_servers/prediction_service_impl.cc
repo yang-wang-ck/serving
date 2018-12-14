39e58473f5690c8a700c845a41659fea1a2f25d3
yang-wang-ck <yang.wang@creditkarma.com>
2018-12-14 14:34:20 -0800

    test

--- tensorflow_serving/model_servers/prediction_service_impl.cc
+++ tensorflow_serving/model_servers/prediction_service_impl.cc
@@ -105,7 +105,8 @@ int DeadlineToTimeoutMillis(const gpr_timespec deadline) {
     return end_microseconds - start_microseconds;
   }();
 
-  RecordRequestExampleCount(&request.model_spec().name(), num_examples);
+  const ClassificationRequest *request_test = &request
+  RecordRequestExampleCount(request_test->model_spec()->name(), num_examples);
 
   return status;
 }
